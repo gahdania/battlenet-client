7a9b03601597fede94d4c5218cbfad50dc1dc438
gahdania <gahdania@gahd.io>
2022-02-02 22:55:47 -0500

    Made changes to structure and import statements to match the refactoring and migration

--- src/battlenet_client/d3/community.py
+++ src/battlenet_client/d3/community.py
@@ -3,7 +3,6 @@ from typing import Optional, Any, TYPE_CHECKING, Dict, Union
 if TYPE_CHECKING:
     from client import D3Client
 
-from decorators import verify_client
 from urllib.parse import quote
 
 
@@ -11,7 +10,6 @@ class CommunityAPI:
     def __init__(self, client: "D3Client") -> None:
         self.client = client
 
-    @verify_client
     def act(
         self, locale: str, act_id: Optional[Union[str, int]] = None
     ) -> Dict[str, Any]:
@@ -21,11 +19,9 @@ class CommunityAPI:
 
         return self.client.community(locale, "act")
 
-    @verify_client
     def artisan(self, locale: str, artisan_name: str) -> Dict[str, Any]:
         return self.client.community(locale, "artisan", artisan_name)
 
-    @verify_client
     def recipe(
         self, locale: str, artisan_name: str, recipe_slug: str
     ) -> Dict[str, Any]:
@@ -33,17 +29,14 @@ class CommunityAPI:
             locale, "artisan", artisan_name, "recipe", self.client.slugify(recipe_slug)
         )
 
-    @verify_client
     def follower(self, locale: str, follower_slug: str) -> Dict[str, Any]:
         return self.client.community(
             locale, "follower", self.client.slugify(follower_slug)
         )
 
-    @verify_client
     def character_class(self, locale: str, class_slug: str) -> Dict[str, Any]:
         return self.client.community(locale, "hero", self.client.slugify(class_slug))
 
-    @verify_client
     def api_skill(
         self, locale: str, class_slug: str, skill_slug: str
     ) -> Dict[str, Any]:
@@ -55,7 +48,6 @@ class CommunityAPI:
             self.client.slugify(skill_slug),
         )
 
-    @verify_client
     def item_type(self, locale: str, item_slug: Optional[str] = None) -> Dict[str, Any]:
         if item_slug:
             return self.client.community(
@@ -64,23 +56,18 @@ class CommunityAPI:
 
         return self.client.community(locale, "item-type")
 
-    @verify_client
     def item(self, locale: str, item_slug: str) -> Dict[str, Any]:
         return self.client.community(locale, "item", item_slug)
 
-    @verify_client
     def api_account(self, locale: str, b_tag: str) -> Dict[str, Any]:
         return self.client.profile_api(locale, f"{quote(b_tag)}/")
 
-    @verify_client
     def api_hero(self, locale: str, b_tag: str, hero_id: str) -> Dict[str, Any]:
         return self.client.profile_api(locale, quote(b_tag), "hero", hero_id)
 
-    @verify_client
     def api_hero_items(self, locale: str, b_tag: str, hero_id: str) -> Dict[str, Any]:
         return self.client.profile_api(locale, quote(b_tag), "hero", hero_id, "items")
 
-    @verify_client
     def api_follower_items(
         self, locale: str, b_tag: str, hero_id: str
     ) -> Dict[str, Any]:
