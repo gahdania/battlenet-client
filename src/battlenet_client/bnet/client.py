4b2b08b27dfde5fee8880a99fa73aaa7a2e89af9
gahdania <gahdania@gahd.io>
2022-02-27 22:03:45 -0500

    added openid connect support.  moved Credential client support to celery task

--- src/battlenet_client/bnet/client.py
+++ src/battlenet_client/bnet/client.py
@@ -4,20 +4,26 @@ Disclaimer:
     All rights reserved, Blizzard is the intellectual property owner of Battle.net and any data
     retrieved from this API.
 """
-from requests import Response
-from requests.exceptions import HTTPError
-from time import sleep
+from oic.oic import Client
+from oic.utils.authn.client import CLIENT_AUTHN_METHOD
+from oic.oic.message import (
+    ProviderConfigurationResponse,
+    RegistrationResponse,
+    AuthorizationResponse,
+)
+from oic import rndstr
+from oic.oauth2 import REQUEST2ENDPOINT
+from oic.oauth2.message import CCAccessTokenRequest
 from decouple import config
 from urllib.parse import unquote
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
+
 from typing import Optional, List
 
 from . import exceptions, constants
 from battlenet_client import utils
 
 
-class BNetClient(OAuth2Session):
+class BNetClient(Client):
     """Handles the communication using OAuth v2 client to the Battle.net REST API
 
     Args:
@@ -33,9 +39,8 @@ class BNetClient(OAuth2Session):
         tag (str): the region tag (abbreviation) of the client
     """
 
-    __MAJOR__ = 2
-    __MINOR__ = 1
-    __PATCH__ = 1
+    __author__ = 'David "Gahd" Couples'
+    __version__ = "3.0.0"
 
     def __init__(
         self,
@@ -61,69 +66,86 @@ class BNetClient(OAuth2Session):
             else:
                 raise exceptions.BNetRegionNotFoundError("Region not available")
 
-        if redirect_uri and scope:
-            super().__init__(
-                client_id=client_id, scope=scope, redirect_uri=redirect_uri
+        if not scope:
+            REQUEST2ENDPOINT[
+                "CCAccessTokenRequest"
+            ] = "https://us.battle.net/oauth/token"
+
+        super().__init__(client_id=client_id, client_authn_method=CLIENT_AUTHN_METHOD)
+        self._state = rndstr()
+        self._nonce = rndstr()
+
+        op_data = {
+            "version": "1.0",
+            "issuer": "https://us.battle.net/oauth",
+            "token_endpoint": "https://us.battle.net/oauth/token",
+        }
+
+        if scope:
+            op_data.update(
+                {
+                    "authorization_endpoint": "https://us.battle.net/oauth/authorize",
+                    "userinfo_endpoint": "https://us.battle.net/oauth/userinfo",
+                }
             )
-        else:
-            super().__init__(client=BackendApplicationClient(client_id=client_id))
-            self.fetch_token(
-                token_url=f"{utils.auth_host(self.tag)}/oauth/token",
-                client_id=client_id,
-                client_secret=client_secret,
+
+            if "openid" in scope:
+                op_data.update({"jwks_uri": "https://us.battle.net/oauth/jwks/certs"})
+
+        op_info = ProviderConfigurationResponse(**op_data)
+        info = {
+            "client_id": "59bdfd31c9fd40689e3d0b0a8652692c",
+            "client_secret": "67twDpFpruwpMUyQ56AfYE0YVxeoa40j",
+        }
+        self.handle_provider_config(op_info, op_info["issuer"])
+        client_reg = RegistrationResponse(**info)
+        self.store_registration_info(client_reg)
+
+        if not scope:
+            args = {
+                "response_type": "code",
+            }
+
+            token_r = self.do_any(
+                CCAccessTokenRequest,
+                request_args=args,
+                endpoint="https://us.battle.net/oauth/token",
+                authn_method="client_secret_basic",
             )
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__} {self.tag.upper()} {self.version} API Client"
 
-    def __repr__(self) -> str:
-        return f"{self.__str__()} ({'Auth Code Flow' if self.auth_code else 'Credential Client Flow'})"
-
-    @property
-    def auth_code(self):
-        return self._client.grant_type == "authorization_code"
-
     @property
     def version(self):
-        return f"v{self.__MAJOR__}.{self.__MINOR__}.{self.__PATCH__}"
+        return f"v{self.__version__}"
 
-    def validate_token(self) -> bool:
-        """Checks with the API if the token is good or not.
-
-        Returns:
-            bool: True of the token is valid, false otherwise.
-        """
-
-        url = f"{utils.auth_host(self.tag)}/oauth/check_token"
-        retry = 0
-        while retry < 5:
-            try:
-                data = self.post(
-                    url,
-                    params={"token": self._client.access_token},
-                    headers={"Battlenet-Namespace": None},
-                )
-                data.raise_for_status()
-            except HTTPError as err:
-                if err.response.status_cod == 429:
-                    sleep(1)
-                    retry += 1
-            else:
-                return (
-                    data.status_code == 200
-                    and data.json()["client_id"] == self.client_id
-                )
-
-    def authorization_url(self, **kwargs) -> str:
+    def authorization_url(self, **kwargs):
         """Prepares and returns the authorization URL to the Battle.net authorization servers
 
         Returns:
             str: the URL to the Battle.net authorization server
         """
-        if not self.auth_flow:
-            raise ValueError("Requires Authorization Workflow")
+        args = {
+            "client_id": self.client_id,
+            "response_type": "code",
+            "scope": ["openid", "wow.profile"],
+            "nonce": self._nonce,
+            "redirect_uri": "dummy",
+            "state": self._state,
+        }
 
-        authorization_url, self._client.state = super().authorization_url(
-            url=f"{utils.auth_host(self.tag)}/oauth/authorize", **kwargs
+        auth_req = self.construct_AuthorizationRequest(request_args=args, **kwargs)
+        return auth_req.request(self.authorization_endpoint)
+
+    def authorization_response(self, response: str):
+        aresp = self.parse_response(
+            AuthorizationResponse, info=response, sformat="urlencoded"
         )
-        return unquote(authorization_url)
+        assert aresp["state"] == self._state
+
+        args = {"code": aresp["code"]}
+
+        resp = self.do_access_token_request(
+            state=aresp["state"], request_args=args, authn_method="client_secret_basic"
+        )
